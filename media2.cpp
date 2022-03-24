8af39be6ddb252f62041bacd0614eda0bc24fd8e
alx47 <101833866+alx47@users.noreply.github.com>
2022-03-24 13:39:35 -0300

    Create media2.cpp

---
+++ media2.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    float nota1, nota2, media;
+
+    
+    cout<< "Nota1..: ";
+    cin>> nota1;
+    cout<< "Nota2..: ";
+    cin>> nota2;
+    
+    media = (nota1+nota2)/2 ; 
+    
+    cout<< "Media..: " << media;
+    
+    
+    
+    
+
+;    return 0;
+}
