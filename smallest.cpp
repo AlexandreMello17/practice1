d297f746a9500a7581fc2b713374a61387d4e0f0
alx47 <101833866+alx47@users.noreply.github.com>
2022-04-12 00:12:42 -0300

    Create smallest.cpp

---
+++ smallest.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    float n1, n2, mn;
+    
+    cout<< "Primeiro número..:";
+    cin>> n1;
+    cout<< "Segundo número..:";
+    cin>> n2;
+    
+    
+    if (n1<n2) {cout<< n1;}
+    
+    else {cout<<n2;}
+
+    return 0;
+}
